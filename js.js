b9252fec754265b3ce15020cbc7f64c4e927a0b6
Vlad <m.obey@bk.ru>
2022-04-27 20:29:12 +0300

    home for phone section added

--- js.js
+++ js.js
@@ -2,17 +2,20 @@ let burger = document.querySelector(".header__burger")
 let BurgerActive = false
 
 burger.addEventListener('click', ()=> {
-	if (BurgerActive == false) {
-		document.getElementsByClassName("header__burger")[0].style="margin-right: 150px"
-		document.getElementsByClassName("menu")[0].style="display: flex"
-		BurgerActive = true
-		console.log(BurgerActive)
+	if (document.documentElement.clientWidth < 922) {
+		if (BurgerActive == false) {
+			document.getElementsByClassName("header__burger")[0].style="margin-right: 150px"
+			document.getElementsByClassName("menu")[0].style="display: flex"
+			BurgerActive = true
+			console.log(BurgerActive)
+		}
+		else if (BurgerActive == true) {
+			document.getElementsByClassName("header__burger")[0].style="margin-right: 0px"
+			setTimeout(timer, 450)
+			BurgerActive = false
+		}
 	}
-	else if (BurgerActive == true) {
-		document.getElementsByClassName("header__burger")[0].style="margin-right: 0px"
-		setTimeout(timer, 450)
-		BurgerActive = false
-	}
+	
 	
 	
 })
